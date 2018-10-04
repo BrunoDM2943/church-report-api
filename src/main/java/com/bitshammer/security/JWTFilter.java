ff3101225695c3049d3277494a16ac29495a663e
BrunoDM2943 <bdm2943@gmail.com>
2018-10-03 22:01:52 -0300

    Validando o token

--- src/main/java/com/bitshammer/security/JWTFilter.java
+++ src/main/java/com/bitshammer/security/JWTFilter.java
@@ -8,23 +8,29 @@ import com.bitshammer.infra.RestCall;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import lombok.Data;
+import spark.Request;
+import spark.Response;
 
+import javax.inject.Singleton;
 import java.io.ByteArrayInputStream;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateFactory;
 import java.security.interfaces.RSAPublicKey;
 import java.util.List;
 
+import static spark.Spark.halt;
+
+@Singleton
 public class JWTFilter {
 
-    public static void main(String[] args) {
-        JWTFilter filter = new JWTFilter();
-        System.out.println(filter.validateToken());
-    }
 
-    private boolean validateToken(){
-        String tokenStr = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik1qRkNORUZFUmtWQ1JVTkdNMFkzTlRBM09UVkdRMEpFUWtKQlFUWkdORFpDT0VKQ01FTkZNZyJ9.eyJpc3MiOiJodHRwczovL2NodXJjaHMuYXV0aDAuY29tLyIsInN1YiI6IjYyQTdKd0lTSG92NGxUMEZIclZyV1RGbm54eHVmWEJKQGNsaWVudHMiLCJhdWQiOiJodHRwczovL2NodXJjaC1tZW1iZXJzLWFwaSIsImlhdCI6MTUzNzEzNTc0NiwiZXhwIjoxNTM3MjIyMTQ2LCJhenAiOiI2MkE3SndJU0hvdjRsVDBGSHJWcldURm5ueHh1ZlhCSiIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.evtcGS27dZdM3tJWBBOaMrUQopzLcFsQnUsrav6Odi1nahEMbXrorWFzV2X-mg-SKU5_NvPqbB7xz1QznG1AEa1KNNxOeFpNrSHzjEULnqQyFFG-qTnUbbNL--_qRtKVcmihPTApp0pacnwl8L2Gd3FkacbeA4pDvfD6EMXlYJCzd6kIyAvTuz-s8cqfQM_ErhaDrqLNsKrOdDBRvKuLMhmdMAPDJlR3b5yAW9JJmHyYUgJKdgYkao415pvu4MrkoLKrsbM5tbIzyMnV8ox9m83X7k5YwHNdJdgGdxisFieXhHoV6oQBtwNL3jUzFRPfmfx-mt1Cs5wsxXXBIwMH8Q";
+    public void validateToken(Request request, Response response) throws Exception{
+        String tokenStr = request.headers("Authorization");
+        if(tokenStr == null) {
+            halt(403, "Unauthorized");
+        }
         try {
+            tokenStr = tokenStr.split(" ")[1];
             DecodedJWT token = JWT.decode(tokenStr);
             RSAPublicKey publicKey = getPublicKey(token.getKeyId());
             JWTVerifier verifier = JWT.require(Algorithm.RSA256(publicKey))
@@ -32,11 +38,11 @@ public class JWTFilter {
                     .withIssuer("https://churchs.auth0.com/")
                     .build(); //Reusable verifier instance
             verifier.verify(tokenStr);
-            return true;
+
         } catch (Exception e){
             e.printStackTrace();
+            halt(403, "Unauthorized");
         }
-        return false;
     }
 
     private RSAPublicKey getPublicKey(String keyId) throws Exception {
