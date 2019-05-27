3186fc82523112e1a93c55b44330142179dba40c
BrunoDM2943 <bdm2943@gmail.com>
2019-05-27 18:20:08 -0300

    Adding tests

--- src/main/java/com/bitshammer/client/churchMembers/MembersSearchResponse.java
+++ src/main/java/com/bitshammer/client/churchMembers/MembersSearchResponse.java
@@ -1,17 +1,23 @@
 package com.bitshammer.client.churchMembers;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MembersSearchResponse {
 
     private String classificacao;
     private Pessoa pessoa;
 
     @Data
+    @Builder
     public static class Pessoa {
         private String nome;
         private String conjuge;
@@ -21,13 +27,17 @@ public class MembersSearchResponse {
         private Contato contato;
 
         @Data
-        public class Contato{
+        @AllArgsConstructor
+        @NoArgsConstructor
+        public static class Contato{
             private String telefone;
             private String celular;
             private String email;
         }
 
         @Data
+        @AllArgsConstructor
+        @NoArgsConstructor
         public static class Endereco {
             private String endereco;
         }
---
+++ src/test/java/com/bitshammer/reports/TestReportsService.java
@@ -0,0 +1,49 @@
+package com.bitshammer.reports;
+
+
+import com.bitshammer.client.churchMembers.ChurchMembersAPI;
+import com.bitshammer.client.churchMembers.MembersSearchResponse;
+import com.bitshammer.reports.jasper.JasperService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestReportsService {
+
+    @InjectMocks
+    private ReportsService reportsService;
+
+    @Mock
+    private ChurchMembersAPI churchMembersAPI;
+
+    @Mock
+    private JasperService jasperService;
+
+    @Test
+    public void testGenerateMembersReport() throws Exception {
+        List<MembersSearchResponse> members = new ArrayList<>();
+        ;
+        IntStream.range(0, 100).forEach(i -> members.add(new MembersSearchResponse("Adulto",
+                MembersSearchResponse.Pessoa.builder()
+                        .nome("Teste")
+                        .contato(new MembersSearchResponse.Pessoa.Contato("1112341234", "1112341234", "email@xicas.com"))
+                        .endereco(new MembersSearchResponse.Pessoa.Endereco("Rua de teste"))
+                        .dtNascimento(LocalDate.now())
+                        .build())));
+        Mockito.when(churchMembersAPI.getMembers()).thenReturn(members);
+        Mockito.when(jasperService.generateReport(ArgumentMatchers.anyString(), ArgumentMatchers.any())).thenCallRealMethod();
+        byte[] bytes = reportsService.generateMembersReport();
+        Assert.assertNotEquals(0, bytes.length);
+    }
+}
